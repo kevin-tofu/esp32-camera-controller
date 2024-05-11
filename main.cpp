2f8d7bdd27f6e1574804ea34cacd93bb6c0267ec
Kevin-Tofu <85769623+kevin-tofu@users.noreply.github.com>
2024-05-11 21:28:05 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -182,79 +182,79 @@ void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
 
   // OV2640 camera module
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sccb_sda = SIOD_GPIO_NUM;
-  config.pin_sccb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  // config.frame_size = FRAMESIZE_UXGA;
-  // config.frame_size = FRAMESIZE_SVGA;
-  config.frame_size = FRAMESIZE_CIF;
-  config.pixel_format = PIXFORMAT_JPEG; // for streaming
-  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-  config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.jpeg_quality = 8;
-  config.fb_count = 1;
+//   camera_config_t config;
+//   config.ledc_channel = LEDC_CHANNEL_0;
+//   config.ledc_timer = LEDC_TIMER_0;
+//   config.pin_d0 = Y2_GPIO_NUM;
+//   config.pin_d1 = Y3_GPIO_NUM;
+//   config.pin_d2 = Y4_GPIO_NUM;
+//   config.pin_d3 = Y5_GPIO_NUM;
+//   config.pin_d4 = Y6_GPIO_NUM;
+//   config.pin_d5 = Y7_GPIO_NUM;
+//   config.pin_d6 = Y8_GPIO_NUM;
+//   config.pin_d7 = Y9_GPIO_NUM;
+//   config.pin_xclk = XCLK_GPIO_NUM;
+//   config.pin_pclk = PCLK_GPIO_NUM;
+//   config.pin_vsync = VSYNC_GPIO_NUM;
+//   config.pin_href = HREF_GPIO_NUM;
+//   config.pin_sccb_sda = SIOD_GPIO_NUM;
+//   config.pin_sccb_scl = SIOC_GPIO_NUM;
+//   config.pin_pwdn = PWDN_GPIO_NUM;
+//   config.pin_reset = RESET_GPIO_NUM;
+//   config.xclk_freq_hz = 20000000;
+//   // config.frame_size = FRAMESIZE_UXGA;
+//   // config.frame_size = FRAMESIZE_SVGA;
+//   config.frame_size = FRAMESIZE_CIF;
+//   config.pixel_format = PIXFORMAT_JPEG; // for streaming
+//   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+//   config.fb_location = CAMERA_FB_IN_PSRAM;
+//   config.jpeg_quality = 8;
+//   config.fb_count = 1;
 
-  if (config.pixel_format == PIXFORMAT_JPEG)
-  {
-    if (psramFound())
-    {
-      config.jpeg_quality = 5;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    }
-    else
-    {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  }
-  else
-  {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_QVGA;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
+//   if (config.pixel_format == PIXFORMAT_JPEG)
+//   {
+//     if (psramFound())
+//     {
+//       config.jpeg_quality = 5;
+//       config.fb_count = 2;
+//       config.grab_mode = CAMERA_GRAB_LATEST;
+//     }
+//     else
+//     {
+//       // Limit the frame size when PSRAM is not available
+//       config.frame_size = FRAMESIZE_SVGA;
+//       config.fb_location = CAMERA_FB_IN_DRAM;
+//     }
+//   }
+//   else
+//   {
+//     // Best option for face detection/recognition
+//     config.frame_size = FRAMESIZE_QVGA;
+// #if CONFIG_IDF_TARGET_ESP32S3
+//     config.fb_count = 2;
+// #endif
+//   }
 
-  // Camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    ESP.restart();
-  }
+//   // Camera init
+//   esp_err_t err = esp_camera_init(&config);
+//   if (err != ESP_OK) {
+//     Serial.printf("Camera init failed with error 0x%x", err);
+//     ESP.restart();
+//   }
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send_P(200, "text/html", index_html);
   });
 
-  server.on("/capture", HTTP_GET, [](AsyncWebServerRequest * request) {
-    takeNewPhoto = true;
-    request->send_P(200, "text/plain", "Taking Photo");
-  });
+//   server.on("/capture", HTTP_GET, [](AsyncWebServerRequest * request) {
+//     takeNewPhoto = true;
+//     request->send_P(200, "text/plain", "Taking Photo");
+//   });
 
-  server.on("/saved-photo", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, FILE_PHOTO, "image/jpg", false);
-  });
+//   server.on("/saved-photo", HTTP_GET, [](AsyncWebServerRequest * request) {
+//     request->send(SPIFFS, FILE_PHOTO, "image/jpg", false);
+//   });
 
   // Start server
   server.begin();
@@ -262,10 +262,10 @@ void setup() {
 }
 
 void loop() {
-  if (takeNewPhoto) {
-    capturePhotoSaveSpiffs();
-    takeNewPhoto = false;
-  }
+//   if (takeNewPhoto) {
+//     capturePhotoSaveSpiffs();
+//     takeNewPhoto = false;
+//   }
   delay(100);
 }
 
